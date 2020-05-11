ec1605bff19fb339e5d49704ccec8039205c9219
emma <fm.buapan@gmail.com>
2020-05-12 02:18:39 +0700

    add home page

---
+++ lib/pages/home.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        elevation: 0.2,
+        backgroundColor: Colors.yellow,
+        title: Text('Fat cats'),
+        actions: <Widget>[
+
+        ],
+      ),
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+              accountName: Text('Suparuttanatree Buapan'), 
+              accountEmail: Text('fm.buapan@gmail.com'),
+            currentAccountPicture: GestureDetector(
+              child: new CircleAvatar(
+                backgroundColor: Colors.grey,
+              ),
+            ),
+            decoration: new BoxDecoration(
+              color: Colors.yellow
+            ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Home Page'),
+                leading: Icon(Icons.home, color: Colors.red),
+            )
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('My Account'),
+                leading: Icon(Icons.person, color: Colors.red),
+            )
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('My Orders'),
+                leading: Icon(Icons.shopping_basket, color: Colors.red),
+            )
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Shopping cart'),
+                leading: Icon(Icons.shopping_cart, color: Colors.red),
+            )
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Favourites'),
+                leading: Icon(Icons.favorite, color: Colors.red),
+            )
+            ),
+            Divider(),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Setting'),
+                leading: Icon(Icons.settings, color: Colors.grey),
+            )
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('About'),
+                leading: Icon(Icons.help, color: Colors.blue),
+            )
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
