2a9d6abb67741adf27ef94431b33f166d743d3b2
emma <fm.buapan@gmail.com>
2020-05-22 15:19:39 +0700

    add connection

---
+++ lib/components/connection.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CallConnection extends StatefulWidget {
+  @override
+  _CallConnectionState createState() => _CallConnectionState();
+}
+
+class _CallConnectionState extends State<CallConnection> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
+
+  
   @override
   _RegisterState createState() => _RegisterState();
+
 }
 
 class _RegisterState extends State<Register> {
