eebad6d28d6f004ebfacf8af8682cbfbeb1fb85c
emma <fm.buapan@gmail.com>
2020-06-05 16:11:55 +0700

    answer

--- lib/pages/Answers.dart
+++ lib/pages/Answers.dart
@@ -25,11 +25,53 @@ class _AnswersPageState extends State<AnswersPage> {
       appBar: new AppBar(
         elevation: 0.2,
         backgroundColor: Colors.yellow.shade700,
-        title: Text('Answer',style: TextStyle(fontFamily:'Omyim',fontSize: 50)),
+        title: Text('Answers',style: TextStyle(fontFamily:'Omyim',fontSize: 50)),
         actions: <Widget>[
           new IconButton(
               icon: Icon(Icons.search, color: Colors.white), onPressed: (null)),
         ],
+      ),
+      body: Container(
+        // new Text('\tบทเรียนของฉัน',style: TextStyle(fontSize: 20),),
+        child: ListView(
+          children: <Widget>[      
+            new Text('\tบทเรียนของฉัน',style: TextStyle(fontFamily:'Omyim',fontSize: 40)),
+            Divider(),
+            InkWell(
+                onTap: () {
+                  // Navigator.push(context,
+                  //     MaterialPageRoute(builder: (context) => PlayerPage()));
+                },
+                child: ListTile(
+                  title: Text('ทบทวนเนื้อหาฟิสิกส์',style: TextStyle(fontFamily:'Omyim',fontSize: 30)),
+                  subtitle: Text('data'),
+                  leading: Image.asset('images/products/product7.jpg'),
+                )),
+            Divider(),
+            InkWell(
+                onTap: () {
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => CoursesPage()),
+                  // );
+                },
+                child: ListTile(
+                  title: Text('ติวเข้ม PAT2',style: TextStyle(fontFamily:'Omyim',fontSize: 30)),
+                  subtitle: Text('data'),
+                  leading: Image.asset('images/products/product5.jpg'),
+                )),
+            Divider(),
+            InkWell(
+                onTap: () {},
+                child: ListTile(
+                  title: Text('ติวเข้ม PAT3',style: TextStyle(fontFamily:'Omyim',fontSize: 30)),
+                  subtitle: Text('data'),
+                  
+                  leading: Image.asset('images/products/product6.jpg'),
+                )),
+            Divider(),
+          ],
+        ),
       ),
       );
     
